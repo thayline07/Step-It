ba561466409a2c7b32b58ba18762c9fcf9e00b60
thayline07 <thaylinesimioni@gmail.com>
2025-08-25 00:09:20 -0300

    .

---
+++ Step-It/backend/index.ts
@@ -0,0 +1,86 @@
+export const lightTheme = {
+  colors: {
+    background_principal: "#fff",
+    background_gradient_linear: "#fff",
+    background_gradient_diamond: "#fff",
+    texto: "#000",
+    raio: "#0B1F3A",
+    numero: "#0D385E",
+    numero_legenda: "rgba(13, 56, 94, 0.5)",
+    titulo: "#62AAEA",
+    borda_foto: "#62AAEA",
+    circulo_degrade: "linear-gradient(135deg, #3C9E68, #1F86E0)",
+    circulo_borda: "#0B1F3A",
+    titulo_card: "#2C5C41",
+    texto_principal: "#2C5C41",
+    card: "linear-gradient(135deg, rgba(34, 80, 54, 0.2), rgba(77, 182, 124, 0.2))",
+    borda_card: "#fff",
+    botao_theme: {
+      background: "transparent",
+      borda:
+        "linear-gradient(135deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 1))",
+      sombra: "rgba(0, 0, 0, 0.05)",
+      icone: "#FFF",
+    },
+    navegacao: {
+      background: "#020202",
+      sombra: "#000",
+      borda:
+        "linear-gradient(135deg, rgba(20, 29, 38, 0.69), rgba(20, 29, 38, 0))",
+      icones: "#155E9D",
+      icone_clicado: "#fff",
+      sombra_icone_clicado: "#fff",
+      fundo_icone_clicado: "#1F86E0",
+      sombra_fundo_icone_clicado: "#62AAEA",
+    },
+    perfil: {
+      titulo: "1F86E0",
+      card: "linear-gradient(135deg, rgba(60, 158, 104, 1), rgba(163, 220, 188, 0.72))",
+      link: "#10A3FF",
+    },
+    relatorios: {},
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    background_principal: "#020202",
+    background_gradient_linear: "linear-gradient(135deg, #155E9D, #020202)",
+    background_gradient_diamond:
+      "radial-gradient(circle, #62AAEA, #3871A4, #0D385E)",
+    texto: "#fff",
+    raio: "#1F73E0",
+    numero: "#fff",
+    numero_legenda: "rgba(255, 255, 255, 0.5)",
+    titulo: "#2C5C41",
+    borda_foto: "#3C9E68",
+    circulo_degrade: "linear-gradient(135deg, #3C9E68, #1F86E0)",
+    circulo_borda: "#0B1F3A",
+    titulo_card: "#fff",
+    texto_principal: "#2C5C41",
+    card: "linear-gradient(135deg, rgba(60, 158, 104, 0.1), rgba(163, 220, 188, 0.1))",
+    botao_theme: {
+      background: "transparent",
+      borda: "linear-gradient(135deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1))",
+      sombra: "rgba(0, 0, 0, 0.05)",
+      icone: "#000",
+    },
+    borda_card: "#fff",
+    navegacao: {
+      background: "#fff",
+      sombra: "#000",
+      borda:
+        "linear-gradient(135deg, rgba(31, 134, 224, 0.69), rgba(31, 134, 224, 0))",
+      icones: "#155E9D",
+      icone_clicado: "#fff",
+      sombra_icone_clicado: "#fff",
+      fundo_icone_clicado: "#1F86E0",
+      sombra_fundo_icone_clicado: "#62AAEA",
+    },
+    perfil: {
+      titulo: "1F86E0",
+      card: "linear-gradient(135deg, rgba(60, 158, 104, 0.1), rgba(163, 220, 188, 0.072))",
+      link: "#10A3FF",
+    },
+  },
+};
